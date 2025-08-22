f419b63d2777e0614e43014524a351dfe45a1079
jmegabassd <jmegabassd@gmail.com>
2025-08-22 12:14:13 +0100

    Feat: breakdown and describe tasks for javascript

--- app.js
+++ app.js
@@ -1,1 +1,68 @@
 console.log("MOO!");
+
+//TODO: I need to store my image data
+//you can use images stored locally or remotely
+
+const = images [
+{
+    url: "#",
+    altText: "#",
+},
+{
+    url: "",
+    altText "",
+},
+{
+    url: "",
+    altText "",
+},
+]
+//
+//
+
+//TODO I need to create my thumbnail images
+
+// function createThumbnails(){
+//}
+
+//select the DOM element (thumbnail container) to contain our thumbnails
+
+//this is a repetitive task - loop - kloop through our array using the length property
+
+// inside our loop we need to do this
+
+// first we need to create the image element
+
+//second we need to update the src and alt attributes of the image element to match those in the array parameters
+
+//give each image a className (img.className)
+
+//last we need to append the created images to the thumbnail-container
+
+// we need an event add an event listener to each image - the event handler is the function you wirte to create large images (action to create large images)
+
+
+//TODO: I need to create my large images
+
+//this task is the event handler for the thumbnail events
+function createLargeImages(){ //handler
+
+}
+//select the image container
+
+//delete the current image in the large image container
+
+//largeImageContainer.innerHTML = null OR ""
+
+//create and image
+
+//update the src and alt values
+
+//add a className for styling
+
+//append image to the container
+
+//no loop needed
+
+//addd this event handler to the thumbnail event
+//you call the createThumbnail function
